eb7edd365ee0e862be4d7ced23ff0e0e3d3539dd
Karthik Chikmagalur <karthikchikmagalur@gmail.com>
2021-11-12 18:30:55 -0800

    Add ffmpeg-crop.el

---
+++ ffmpeg-crop.el
@@ -0,0 +1,117 @@
+(require 'transient)
+(require 'seq)
+
+(defvar ffmpeg-crop-infiles nil)
+(defvar ffmpeg-crop-description "")
+
+;; Infixes
+
+(transient-define-argument ffmpeg-crop--i ()
+  "Input file"
+  :class 'transient-option
+  :key "i"
+  :argument "-i "
+  :always-read t
+  :init-value (lambda (obj) (oset obj value (car ffmpeg-crop-infiles)))
+  :description "Input file"
+  :prompt "Input video: "
+  :reader 'ffmpeg-crop--read-file)
+
+(defun ffmpeg-crop--read-file (prompt initial-input history)
+  (interactive)
+  (read-file-name prompt nil nil t initial-input nil))
+
+(transient-define-argument ffmpeg-crop--vf ()
+  "Ending at time"
+  :class 'transient-option
+  :key "-vf"
+  :argument "-vf "
+  :init-value (lambda (obj) (oset obj value "scale='trunc(iw/2):trunc(ih/2)'"))
+  :description "Video filter")
+
+(transient-define-infix ffmpeg-crop--add-description ()
+  :description "Video description"
+  :class 'transient-lisp-variable
+  :variable 'ffmpeg-crop-description
+  :reader '(lambda (prompt initial history)
+             (interactive)
+            (read-from-minibuffer prompt initial nil nil history)))
+
+(defun ffmpeg-crop--read-time (prompt initial-input history)
+  (interactive)
+  (read-from-minibuffer prompt nil nil nil history))
+
+;; Suffixes
+
+(transient-define-suffix ffmpeg-crop--copy ()
+  (interactive)
+  (message "%s" (transient-args 'ffmpeg-crop)))
+
+(transient-define-suffix ffmpeg-crop--next ()
+  "Cancel this file and operate on the next one."
+  (interactive)
+  (setq ffmpeg-crop-description "")
+  (when-let ((infiles (cdr-safe ffmpeg-crop-infiles)))
+    (ffmpeg-crop infiles)))
+
+(transient-define-suffix ffmpeg-crop--run (&optional args)
+  "Call ffmpeg and convert this file"
+  (interactive (list (transient-args 'ffmpeg-crop)))
+  (let* ((infile (seq-some (lambda (arg) (when (string-prefix-p "-i " arg)
+                                      (substring arg 3)))
+                           args))
+         (outfile (replace-regexp-in-string
+                  "\\.mp4$"
+                  (concat "_small"
+                          (if (and ffmpeg-crop-description
+                                   (> (length ffmpeg-crop-description) 0))
+                              (concat "_" 
+                                      (string-join
+                                       (split-string ffmpeg-crop-description)
+                                       "_"))
+                            "")
+                          ".mp4")
+                  infile)))
+    (apply #'start-process
+     "ffmpeg" "ffmpeg-crop" "/usr/bin/ffmpeg"
+     (append (mapcan (lambda (arg)
+                       (string-match "^\\([^[:space:]]+\\) \\(.*\\)" arg)
+                       (list (match-string 1 arg) (match-string 2 arg)))
+                     args)
+             (list "-an" (expand-file-name outfile))))
+    (ffmpeg-crop--next)))
+
+;;;###autoload
+(transient-define-prefix ffmpeg-crop (infiles)
+  "Downsample a video and process it."
+  ["Input File"
+   (ffmpeg-crop--i)
+   ("h" ffmpeg-crop--add-description)]
+  [["Times"
+    ("ss" "From time " "-ss " :class transient-option)
+    ("to" "Until time" "-to " :class transient-option)]
+   ["Filter"
+    ("vf" ffmpeg-crop--vf)]]
+  ["Convert"
+   [("RET" "Convert" ffmpeg-crop--run)
+    ("w" "Copy cmd" ffmpeg-crop--copy :transient t)]
+   [("n" "Next file" ffmpeg-crop--next :if (lambda () (cdr ffmpeg-crop-infiles)))]]
+  (interactive (list (completing-read-multiple
+                      "Videos: "
+                      #'completion--file-name-table
+                      nil t)))
+  (setq ffmpeg-crop-infiles infiles)
+  (let ((infile (car infiles)))
+    (start-process (concat "mpv " (file-name-base infile))
+                   (concat "mpv " (file-name-base infile))
+                   "/usr/bin/mpv"
+                   (expand-file-name infile)))
+  (transient-setup 'ffmpeg-crop))
+
+;;;###autoload
+(defun ffmpeg-crop-dired ()
+  (interactive)
+  (require 'dired)
+  (ffmpeg-crop (dired-get-marked-files)))
+
+(provide 'ffmpeg-crop)
